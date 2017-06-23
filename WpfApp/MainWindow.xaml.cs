7684bb9ce79cc40f9ceca23a3a79c7b7f081c562
khoser <ktuuuu@gmail.com>
2017-06-23 23:01:03 +0400

    Default interface

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -29,27 +29,11 @@ namespace WpfApp
     public partial class MainWindow : Window
     {
         static String URL = "http://localhost/";
-        ToDoItem tdi = new ToDoItem { Key = "1", Name = "Job to do", IsComplete = false };
+        List<ToDoItem> tdis = null;
         static HttpClient client = new HttpClient();
         private bool isclientfilled = false;
 
-        public MainWindow()
-        {
-            InitializeComponent();
-        }
-
-        private void Button_Click(object sender, RoutedEventArgs e)
-        {
-            WebClient client = new WebClient();
-
-            //Get request
-            //                                                                              | GET DATA                   |
-            string response = client.DownloadString("http://localhost/api/todo");
-
-            //var o = new { name = 'w' };
-            //o.name
-        }
-        private void BtnPost_Click(object sender, RoutedEventArgs e)
+        private void CreateClient()
         {
             if (!isclientfilled)
             {
@@ -58,16 +42,101 @@ namespace WpfApp
                 client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
                 isclientfilled = true;
             }
+        }
 
-            RunAsyncPost(tdi);
+        public MainWindow()
+        {
+            InitializeComponent();
+            CreateClient();
+            RunAsyncGet();
+        }
 
-            var o = 1;
-            
+        private void SetVisibility()
+        {
+            int i = 0;
+            //layoutGrid.Children.Clear();
+            for (i = layoutGrid.RowDefinitions.Count; i > 1 ; i--)
+            {
+                layoutGrid.RowDefinitions.RemoveAt(i);
+            }
+
+            i = 1;
+            foreach (ToDoItem tdi in tdis)
+            {
+                RowDefinition rdfn = new RowDefinition();
+                rdfn.Height = FirstRow.Height;
+                layoutGrid.RowDefinitions.Add(rdfn);
+
+                CheckBox chbx = new CheckBox();
+                chbx.IsChecked = tdi.IsComplete;
+                chbx.Tag = tdi.Key;
+                chbx.VerticalAlignment = VerticalAlignment.Center;
+                chbx.HorizontalAlignment = HorizontalAlignment.Center;
+                layoutGrid.Children.Add(chbx);
+                Grid.SetRow(chbx, i);
+                Grid.SetColumn(chbx, 0);
+
+                TextBlock txblk = new TextBlock();
+                txblk.Text = tdi.Name;
+                txblk.Tag = tdi.Key;
+                txblk.VerticalAlignment = VerticalAlignment.Center;
+                layoutGrid.Children.Add(txblk);
+                Grid.SetRow(txblk, i);
+                Grid.SetColumn(txblk, 1);
+
+                i++;
+            }
+
+            RowDefinition rdfn1 = new RowDefinition();
+            rdfn1.Height = FirstRow.Height;
+            layoutGrid.RowDefinitions.Add(rdfn1);
+
+            TextBlock txblki = new TextBlock();
+            txblki.Text = "total count: " + i.ToString();
+            txblki.VerticalAlignment = VerticalAlignment.Center;
+            layoutGrid.Children.Add(txblki);
+            Grid.SetRow(txblki, ++i);
+            Grid.SetColumn(txblki, 1);
+        }
+        
+
+        //private void Button_Click(object sender, RoutedEventArgs e)
+        //{
+        //    try
+        //    {
+        //        List<ToDoItem> ListOfTDI = await GetProductsAsync(URL);
+        //    }
+        //    catch (Exception e)
+        //    {
+        //        //Console.WriteLine(e.Message);
+        //    }
+        //}
+        private void BtnPost_Click(object sender, RoutedEventArgs e)
+        {
+            CreateClient();
+            if (textBox.Text != "")
+            {
+                ToDoItem newToDoitem = new ToDoItem { Name = textBox.Text, IsComplete = (bool)checkBox.IsChecked };
+                RunAsyncPost(newToDoitem);
+            }
+        }
+
+        async Task RunAsyncGet()
+        {
+            try
+            {
+                var url = await GetProductsAsync(URL);
+                tdis = url;
+                SetVisibility();
+            }
+            catch (Exception e)
+            {
+                //Console.WriteLine(e.Message);
+            }
         }
 
         static async Task RunAsyncPost(ToDoItem product)
         {
-            
             try
             {
                 // Create a new product
@@ -79,7 +148,7 @@ namespace WpfApp
             }
         }
 
-            static async Task<ToDoItem> GetProductAsync(string path)
+        static async Task<ToDoItem> GetProductAsync(string path)
         {
             ToDoItem product = null;
             HttpResponseMessage response = await client.GetAsync(path);
@@ -90,11 +159,22 @@ namespace WpfApp
             return product;
         }
 
+        static async Task<List<ToDoItem>> GetProductsAsync(string path)
+        {
+            List<ToDoItem> product = null;
+            HttpResponseMessage response = await client.GetAsync("api/todo").ConfigureAwait(false);
+            if (response.IsSuccessStatusCode)
+            {
+                product = await response.Content.ReadAsAsync<List<ToDoItem>>();
+            }
+
+            return product;
+        }
+
         static async Task<Uri> CreateProductAsync(ToDoItem product)
         {
             HttpResponseMessage response = await client.PostAsJsonAsync("api/todo", product).ConfigureAwait(false);
             response.EnsureSuccessStatusCode();
-
             // Return the URI of the created resource.
             return response.Headers.Location;
         }
@@ -125,7 +205,7 @@ namespace WpfApp
         //    {
         //        // Create a new product
         //        var url = await CreateProductAsync(product);
-                
+
         //        // Get the product
         //        product = await GetProductAsync(url.PathAndQuery);
         //        ShowProduct(product);
