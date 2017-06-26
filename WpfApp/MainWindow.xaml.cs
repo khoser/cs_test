f89e04cf5d084878dc9b505a1ee11890e996011a
khoser <ktuuuu@gmail.com>
2017-06-26 14:04:06 +0400

    Charset UTF8 - russian symbols.

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -1,25 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Collections.Specialized;
-using System.IO;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Headers;
-using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
-using System.Xml.Linq;
 using WpfApp.Models;
-using Newtonsoft.Json;
 
 namespace WpfApp
 {
@@ -29,6 +18,7 @@ namespace WpfApp
     public partial class MainWindow : Window
     {
         static String URL = "http://todo.kter.ru/";
+        //static String URL = "http://localhost:49743";
         List<ToDoItem> tdis = null;
         static HttpClient client = new HttpClient();
         private bool isclientfilled = false;
